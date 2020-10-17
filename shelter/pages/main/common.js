859241c8a9599990fa123059533a9c268787d436
michael-916310 <916310@rambler.ru>
2020-10-17 16:46:32 +0400

    feat: обработка клика по логотипу

---
+++ shelter/pages/main/common.js
@@ -0,0 +1,5 @@
+(function(){
+  // обработаем клик по логотипу
+  document.querySelector('.header-logo__tittle').parentElement.addEventListener('click', ()=>{location.assign('../main/index.html')});
+
+})()
