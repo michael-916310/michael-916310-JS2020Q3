b68fa25fa10e6d2511d6674a5db5621093c017ef
michael-916310 <916310@rambler.ru>
2020-10-18 22:05:55 +0400

    feat: слайдер едет вправо и возвращается

--- shelter/pages/main/slider-main.js
+++ shelter/pages/main/slider-main.js
@@ -25,15 +25,63 @@ function addSlider(petsJson){
     }
 
     let stepDelta = leftStep-rightStep;
-    if (stepDelta<0){
-      let l = Math.round(parseFloat(getComputedStyle(sliderItems[0]).width)*8);
-      sliderItems[sliderItems.length+stepDelta].style.left = `${-l}px`;
+    let line = Math.round(parseFloat(getComputedStyle(sliderItems[0]).width)*8);
 
-      console.log(sliderItems.length+stepDelta,stepDelta, rightStep);
+    if (stepDelta<0){
+
+      if (!toLeft) {
+
+        let kf = 1;
+        let delta = stepDelta;
+
+        if (Math.abs(delta)>=sliderItems.length){
+          kf = kf + Math.floor(Math.abs(delta)/sliderItems.length);
+          delta = delta % sliderItems.length;
+          if (Math.abs(delta)==0) {
+            delta = - sliderItems.length;
+            kf= kf -1;
+          }
+        }
+
+        let shift = line *kf;
+        let idx = sliderItems.length+delta;
+
+        //console.log(`idx:${idx}`, `shift:${shift}`, `delta:${delta}`,`kf:${kf}`, `stepDelta:${stepDelta}`);
+
+        sliderItems[idx].style.left = `${-shift}px`;
+
+      }
     }
 
-    if (stepDelta>0){
+    if (stepDelta<=0){
 
+      if (toLeft) {
+        let maxShift=0;
+
+        sliderItems.forEach((item)=>{
+          let j = parseInt(item.style.left);
+          if (j<maxShift) {
+            maxShift=j;
+          }
+        });
+
+        let minIndex=sliderItems.length;
+        sliderItems.forEach((item, idx)=>{
+          let j = parseInt(item.style.left);
+          if (j==maxShift) {
+            if (idx<minIndex) {
+              minIndex = idx;
+            }
+          }
+        });
+
+        let shift=-parseInt(sliderItems[minIndex].style.left)-line;
+
+        console.log(`maxShift:${maxShift}`, `idx: ${minIndex}`, `shift:${shift}`);
+
+        sliderItems[minIndex].style.left = `${-shift}px`;
+
+      }
     }
 
     sliderItems.forEach((element, index) => {
