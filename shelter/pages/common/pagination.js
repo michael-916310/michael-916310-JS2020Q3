70e6e2fe12872ee9c8778ab300725769d832077d
michael-916310 <916310@rambler.ru>
2020-10-21 23:24:45 +0400

    feat: добавил попап на второй странице

--- shelter/pages/common/pagination.js
+++ shelter/pages/common/pagination.js
@@ -43,7 +43,7 @@
     //console.log(dataArr);
   })();
 
-  // инициализация
+  // инициализация, пагинация, попап
   (function(){
     let allCardList = [...document.querySelectorAll('.pets__card')];
     visibleCardList = allCardList.filter((card)=>{
@@ -82,6 +82,62 @@
     document.querySelector('.pets-container').addEventListener('transitionend', ()=>{
       document.querySelector('.pets-container').classList.remove('pets-container-fadeOut');
     });
+
+
+  // добавим открытие попапов
+  function openPopup(){
+    let bg = document.querySelector('.pets-popup-bg');
+
+    let idx = this.dataset.idx;
+    if (idx) {
+      let dt = petsDataArray[dataArr[idx]];
+      bg.querySelector('.pets-popup__name').innerHTML = dt.name;
+      bg.querySelector('.pets-popup__title').innerHTML = `${dt.type} - ${dt.breed}`;
+      bg.querySelector('.pets-popup__info').innerHTML = dt.description;
+      bg.querySelector('.pets-popup__img').src = dt.img;
+
+      bg.querySelectorAll('.pets-popup__list-item-data')[0].innerHTML = dt.age;
+      bg.querySelectorAll('.pets-popup__list-item-data')[1].innerHTML = dt.inoculations;
+      bg.querySelectorAll('.pets-popup__list-item-data')[2].innerHTML = dt.diseases;
+      bg.querySelectorAll('.pets-popup__list-item-data')[3].innerHTML = dt.parasites;
+
+      bg.classList.add('pets-popup-bg-visible');
+    }
+    // items.forEach((item)=>{
+    //   if (item.element == this.parentNode) {
+    //     let dt = petsDataArray[item.petsArrIndex];
+    //     bg.querySelector('.pets-popup__name').innerHTML = dt.name;
+    //     bg.querySelector('.pets-popup__title').innerHTML = `${dt.type} - ${dt.breed}`;
+    //     bg.querySelector('.pets-popup__info').innerHTML = dt.description;
+    //     bg.querySelector('.pets-popup__img').src = dt.img;
+
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[0].innerHTML = dt.age;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[1].innerHTML = dt.inoculations;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[2].innerHTML = dt.diseases;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[3].innerHTML = dt.parasites;
+
+    //     bg.classList.add('pets-popup-bg-visible');
+    //   }
+    // });
+
+  }
+
+  function closePopup(){
+    document.querySelector('.pets-popup-bg').classList.remove('pets-popup-bg-visible');
+  }
+  function bgClick(e){
+    if (!document.querySelector('.pest-popup').contains(e.target)) {
+      closePopup();
+    }
+  }
+
+  document.querySelectorAll('.pets__card').forEach((el)=>{
+    el.addEventListener('click', openPopup);
+  });
+
+  document.querySelector('.pets-popup__close-btn').addEventListener('click', closePopup);
+  document.querySelector('.pets-popup-bg').addEventListener('click', bgClick);
+
 
   })();
 
@@ -128,6 +184,7 @@
 
         el.querySelector('.pets__card-photo').src = dt.img;
         el.querySelector('.pets__card-caption').innerHTML=dt.name;
+        el.setAttribute('data-idx', idx)
       }
       i++;
 
--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -11,3 +11,52 @@ addBurgerMenu(
   'header-logo__hide',
   isPets = true
 );
+
+
+(function(){
+  let sliderItems = document.querySelectorAll(`.slider-item`);
+  let items = []; // массив элементов
+
+
+
+  // добавим открытие попапов
+  function openPopup(){
+    let bg = document.querySelector('.pets-popup-bg');
+
+    // items.forEach((item)=>{
+    //   if (item.element == this.parentNode) {
+    //     let dt = petsDataArray[item.petsArrIndex];
+    //     bg.querySelector('.pets-popup__name').innerHTML = dt.name;
+    //     bg.querySelector('.pets-popup__title').innerHTML = `${dt.type} - ${dt.breed}`;
+    //     bg.querySelector('.pets-popup__info').innerHTML = dt.description;
+    //     bg.querySelector('.pets-popup__img').src = dt.img;
+
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[0].innerHTML = dt.age;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[1].innerHTML = dt.inoculations;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[2].innerHTML = dt.diseases;
+    //     bg.querySelectorAll('.pets-popup__list-item-data')[3].innerHTML = dt.parasites;
+
+    //     bg.classList.add('pets-popup-bg-visible');
+    //   }
+    // });
+
+  }
+
+  function closePopup(){
+    document.querySelector('.pets-popup-bg').classList.remove('pets-popup-bg-visible');
+  }
+  function bgClick(e){
+    if (!document.querySelector('.pest-popup').contains(e.target)) {
+      closePopup();
+    }
+  }
+
+  document.querySelectorAll('.pets-slider__card').forEach((el)=>{
+    el.addEventListener('click', openPopup);
+  });
+
+  document.querySelector('.pets-popup__close-btn').addEventListener('click', closePopup);
+  document.querySelector('.pets-popup-bg').addEventListener('click', bgClick);
+
+
+})();
