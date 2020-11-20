e23aa1e33a1eb9b79a4e8fc4158968aedce0734f
michael-916310 <916310@rambler.ru>
2020-11-20 12:16:16 +0400

    feat: страница статистики стоится

--- english-for-kids/src/js/statisticPage.js
+++ english-for-kids/src/js/statisticPage.js
@@ -12,21 +12,35 @@ function addToDOM(){
   const fr = document.createDocumentFragment();
 
   categoryData.forEach((catData, catKey)=>{
-    console.log(`${catData} ${catKey}`);
+    const catName = categoryList.filter((el)=>{
+      return (el.id === catKey);
+    })[0].itemName;
     catData.forEach((item)=>{
       const tr = document.createElement('tr');
       tr.classList.add('statistic-table__line');
       tr.insertAdjacentHTML(`beforeend`,`
-        <td class="statistic-table__cell">Action (set A)</td>
-        <td class="statistic-table__cell">${item.word}</td>
-        <td class="statistic-table__cell">${item.translation}</td>
-        <td class="statistic-table__cell statistic-table__cell_center">25</td>
-        <td class="statistic-table__cell statistic-table__cell_center">12</td>
-        <td class="statistic-table__cell statistic-table__cell_center">24</td>
-        <td class="statistic-table__cell statistic-table__cell_center">33%</td>
+        <td class="statistic-table__row-name-line-1">${catName}</td>
+        <td rowspan="3" class="statistic-table__cell statistic-table__cell_center">25</td>
+        <td rowspan="3" class="statistic-table__cell statistic-table__cell_center">12</td>
+        <td rowspan="3" class="statistic-table__cell statistic-table__cell_center">24</td>
+        <td rowspan="3" class="statistic-table__cell statistic-table__cell_center">33%</td>
       `);
-
       fr.append(tr);
+
+      const tr2 = document.createElement('tr');
+      tr2.classList.add('statistic-table__line');
+      tr2.insertAdjacentHTML(`beforeend`,`
+        <td class="statistic-table__row-name-line-2">${item.word}</td>
+      `);
+      fr.append(tr2);
+
+      const tr3 = document.createElement('tr');
+      tr3.classList.add('statistic-table__line');
+      tr3.insertAdjacentHTML(`beforeend`,`
+        <td class="statistic-table__row-name-line-3">${item.translation}</td>
+      `);
+      fr.append(tr3);
+
     })
   })
 
